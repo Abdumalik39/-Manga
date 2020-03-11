f6db176e6c3e2270b7ae6cc428ec11a36c45b7ae
Abdumalik39 <62023882+Abdumalik39@users.noreply.github.com>
2020-03-11 20:45:32 +0600

    Create javs.js

---
+++ butakoushaku/javs.js
@@ -0,0 +1,17 @@
+$(function() {
+    $("#p-caption-hide").click(function() {
+        $(".hidden-area").css("display", "none");
+
+        $("#p-caption-hide").css("display", "none");
+
+        $("#p-caption-display").css("display", "block");
+    });
+
+    $("#p-caption-display").click(function() {
+        $(".hidden-area").css("display", "block");
+
+        $("#p-caption-hide").css("display", "block");
+
+        $("#p-caption-display").css("display", "none");
+    });
+});

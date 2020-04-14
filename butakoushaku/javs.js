be072da10b188a00d5612b00b696303d853431b4
Abdumalik39 <62023882+Abdumalik39@users.noreply.github.com>
2020-04-15 00:05:01 +0600

    10

--- butakoushaku/javs.js
+++ butakoushaku/javs.js
@@ -1,17 +1,35 @@
-$(function() {
-    $("#p-caption-hide").click(function() {
-        $(".hidden-area").css("display", "none");
-
-        $("#p-caption-hide").css("display", "none");
-
-        $("#p-caption-display").css("display", "block");
-    });
-
-    $("#p-caption-display").click(function() {
-        $(".hidden-area").css("display", "block");
-
-        $("#p-caption-hide").css("display", "block");
-
-        $("#p-caption-display").css("display", "none");
-    });
-});
+/**
+ 03
+ * Функция Скрывает/Показывает блок
+ 04
+ * @author ox2.ru дизайн студия
+ 05
+ **/
+06
+function showHide(element_id) {
+    07
+    //Если элемент с id-шником element_id существует
+    08
+    if (document.getElementById(element_id)) {
+        09
+        //Записываем ссылку на элемент в переменную obj
+        10
+        var obj = document.getElementById(element_id);
+        11
+        //Если css-свойство display не block, то:
+        12
+        if (obj.style.display != "block") {
+            13
+            obj.style.display = "block"; //Показываем элемент
+            14
+        }
+        15
+    else obj.style.display = "none"; //Скрываем элемент
+        16
+    }
+    17
+    //Если элемент с id-шником element_id не найден, то выводим сообщение
+    18
+else alert("Элемент с id: " + element_id + " не найден!");
+    19
+}
